7fefc116450c6e18e9002eafa5c714a230312f4c
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-04 07:25:19 -0600

    Update 6-Understanding-Case-Sensitivity-in-Variables.js
    * Declarations of variable with method camelCase.
    * Assignments value to variables and change variable with method camelCase.

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,9 @@
+// Declarations of variable with method camelCase.
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments value to variables and change variable with method camelCase.
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
