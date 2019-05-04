e1225d38511d8c40c1585861945a0b5e8070eb61
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-04 07:39:53 -0600

    Update 8-Subtract-One-Number-from-Another-with-JavaScript.js
    The operation was just change the number at variable difference but I did think than I needed declare variable

---
+++ 5-Introduction-to-JavaScript/8-Subtract-One-Number-from-Another-with-JavaScript.js
@@ -0,0 +1,5 @@
+// var a = 33; 
+var difference = 45 - 33;
+//difference = 45 - a;
+
+//The operation was just change the number at variable difference but I did think than I needed declare variable.
