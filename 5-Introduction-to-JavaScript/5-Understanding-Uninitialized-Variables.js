833f358374e0b316cb1860a620790f52719760e9
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-03 21:36:54 -0600

    Update 5-Understanding-Uninitialized-Variables.js
    Assign value for two variables and put the third variable like a "text", and solve the equation

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,11 @@
+var a;
+var b;
+var c;
+var a = 5; //Assign value for two variables and put the third variable like a "text", and solve the equation 
+var b = 10;
+var c = "I am a"
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
