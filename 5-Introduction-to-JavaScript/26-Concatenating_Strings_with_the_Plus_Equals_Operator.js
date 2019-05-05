df07890211da003e1ab81170c23b37b65b9c9adf
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-04 21:40:39 -0600

    Update 26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
    Concatenating Strings with the Plus Equals Operator

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,10 @@
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
+//I am learning to Concatenating Strings with the Plus Equals Operator. the first line always have a space between point and the quotes.
