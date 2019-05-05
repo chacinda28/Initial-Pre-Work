64fc5e76849d92f55a4453d170815fc6d98026fc
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-04 20:58:34 -0600

    Update 24-Escape-Sequences-in-Strings.js
    Declare a variable in escape sequences in string

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,13 @@
+var myStr = "FirstLine\n\t\\SecondLine\n\ThirdLine"; // Change this line
+// I learned how to declare a variable in escape sequences in string..
+
+
+/*        \'	single quote
+          \"	double quote
+          \\	backslash
+          \n	newline
+          \r	carriage return
+          \t	tab
+          \b	backspace
+          \f	form feed
+*/
