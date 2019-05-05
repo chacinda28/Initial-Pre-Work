184c73ef3545a2c0337ddc9b55df4589b12a0310
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-05 01:02:50 -0600

    Update 52-Understanding_Undefined_Value_returned_from_a_Function.js
    Understanding Undefined Value returned from a Function

---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,14 @@
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive () {
+sum = sum + 5;
+}
+// Only change code above this line
+var returnedValue = addFive();
+
+// I can't put on comment because is confuse for me. I need more practice to understand better.
