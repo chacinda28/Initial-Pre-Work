c3495a295d83ca1bf62517c706b035b25c5b7aa0
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-05 00:47:51 -0600

    Update 50-Global_vs._Local_Scope_in_Functions.js
    Global vs. Local Scope in Functions.

---
+++ 5-Introduction-to-JavaScript/50-Global_vs._Local_Scope_in_Functions.js
@@ -0,0 +1,13 @@
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  var myOutfit;                         // Declare Variable inside the function and return a value.
+  myOutfit = "sweater";
+  return myOutfit;
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
