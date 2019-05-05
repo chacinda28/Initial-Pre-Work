af66904cd7a03e11f05536de9443fce5f7c6bffa
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-04 21:47:20 -0600

    Update 27-Constructing_Strings_with_Variables.js
    Constructing Strings with Variables using operators

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,9 @@
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "David";
+var myStr = "Hello, my name is " + myName + ", and I am well";
+
+// I am learning to Constructing Strings with Variables using operators.
