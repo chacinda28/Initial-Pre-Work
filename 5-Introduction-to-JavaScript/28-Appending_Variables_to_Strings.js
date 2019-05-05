289ec2e61a863a88f73e6e1b535e1cb64554b19e
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-04 21:53:44 -0600

    Update 28-Appending_Variables_to_Strings.js
    Appending Variables to Strings

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,12 @@
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "Incredible";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
+
+//Use the Appending Variables to Strings.
