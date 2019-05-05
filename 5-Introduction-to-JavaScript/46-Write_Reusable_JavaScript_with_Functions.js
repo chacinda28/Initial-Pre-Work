e205829c2e72494e00628280fcc97c8e06b1db78
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-05 00:21:56 -0600

    Update 46-Write_Reusable_JavaScript_with_Functions.js
    Write Reusable JavaScript with Functions

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,12 @@
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction() {     //create function and called the function with de console.log.
+    console.log("Hi World");
+}
+reusableFunction();                                 //Do not forget declare the variable.
