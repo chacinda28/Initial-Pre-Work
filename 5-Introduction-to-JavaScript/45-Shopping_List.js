4c125492d1298288a2da205cbd16f7a45e3b57c4
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-05 00:04:30 -0600

    Update 45-Shopping_List.js
    Shopping List

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,9 @@
+var myList =[ 
+    ["Cajas chocolate blanco", 20],
+    ["Cajas chocolate negro", 5],
+    ["trigo", 10],
+    ["Miel", 5],
+    ["Agua", 25]
+];
+
+// Shopping list and create Variable with 5 arrays.
