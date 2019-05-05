5556c1d882bd7a1f91428f10d1fbc59262845904
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-04 22:12:02 -0600

    Update 32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
    Use Bracket Notation to Find the Nth Character in a String.js

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,11 @@
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[1];
+thirdLetterOfLastName = "v";
+ //Find the Nth character in a string and assign value to the variable.
