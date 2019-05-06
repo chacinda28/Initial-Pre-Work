959446ae305bc09f7fb3e7fa9a1a015b5af70451
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-05 22:04:24 -0600

    Update 63-Comparison_with_the_Greater_Than_Or_Equal_To_Operator.js
    Greater Than Or Equal To Operator

---
+++ 5-Introduction-to-JavaScript/63-Comparison_with_the_Greater_Than_Or_Equal_To_Operator.js
@@ -0,0 +1,14 @@
+function testGreaterOrEqual(val) { 
+  if (val >= 20 ) {  // Change this line // The Use the Greater Than Or Equal To Operator.
+    return "20 or Over";
+  }
+  
+  if (val >= 10 ) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+// Change this value to test
+testGreaterOrEqual(10);
