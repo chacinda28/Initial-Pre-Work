adced8297d781d666bce7e9695f2eeca2de7e8a7
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-05 21:50:17 -0600

    Update 60-Comparison_with_the_Inequality_Operator.js
    Inequality Operator

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,10 @@
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line  //Use the operator Inequality (!=).
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(10);
