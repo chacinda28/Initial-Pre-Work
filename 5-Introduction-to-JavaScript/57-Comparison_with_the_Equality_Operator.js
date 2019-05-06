63c0d3f8da8eb3f2378b692d927e451de58754cf
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-05 21:38:43 -0600

    Update 57-Comparison_with_the_Equality_Operator.js
    Comparison with the Equality Operator

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,10 @@
+// Setup
+function testEqual(val) {
+  if (val == 12 ) { // Change this line   //I am learning the use the operator equal with the conditional if.
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);
