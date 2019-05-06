08cd19f1988e9a8bcbb2e73b101e768f24da7165
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-05 22:23:10 -0600

    Update 66-Comparisons_with_the_Logical_And_Operator.js
    Logical And Operator

---
+++ 5-Introduction-to-JavaScript/66-Comparisons_with_the_Logical_And_Operator.js
@@ -0,0 +1,15 @@
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >= 25) {  // Comparisons with the Logical And Operator using if just once.
+  
+      return "Yes";
+    }
+  
+
+  // Only change code above this line
+  return "No";
+}
+
+// Change this value to test
+testLogicalAnd(10);
