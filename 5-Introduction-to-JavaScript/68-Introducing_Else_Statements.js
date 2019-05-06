85f92828947d40c0ddfb8508c92d7caf032bd5bd
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-05 22:36:31 -0600

    Update 68-Introducing_Else_Statements.js
    Introducing Else Statements

---
+++ 5-Introduction-to-JavaScript/68-Introducing_Else_Statements.js
@@ -0,0 +1,18 @@
+function testElse(val) {
+  var result = "";
+  // Only change code below this line
+  
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+  
+  else {                                //Use the else after if.
+    result = "5 or Smaller";
+  }
+  
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+testElse(4);
