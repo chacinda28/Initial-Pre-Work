9f606e5b80a2c574f820ab3b9b1b8f1f1a78b5e5
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-06 19:52:47 -0600

    Update 78-Return_Early_Pattern_for_Functions.js
    Return Early Pattern for Functions

---
+++ 5-Introduction-to-JavaScript/78-Return_Early_Pattern_for_Functions.js
@@ -0,0 +1,17 @@
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+  if (a < 0 || b < 0){
+    return undefined
+  }
+  
+  
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+// Change values below to test your code
+abTest(2,2);
+
+// I understand all code but I do not understand this activity "(abTest(2,2) should return 8)".
