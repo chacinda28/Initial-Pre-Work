54e900e3c0b6903618c4035caf660ba29a930920
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-06 21:37:04 -0600

    Update 89-Manipulating_Complex_Objects.js
    Manipulating Complex Objects

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,26 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+   { 
+    "artist": "David chacin",
+    "title": "Learning",
+    "release_year": 2019,
+    "formats": [ 
+      "CD",
+      "8T",
+      "MP4"],
+      
+   },
+];
+
+//Manipulating Complex Objects with strings
