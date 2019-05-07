ce5f2c9541ffa02974ea9d5b400a79b8d0a2b8bc
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-06 21:04:13 -0600

    Update 86-Delete_Properties_from_a_JavaScript_Object.js
    Delete Properties from a JavaScript Object

---
+++ 5-Introduction-to-JavaScript/86-Delete_Properties_from_a_JavaScript_Object.js
@@ -0,0 +1,22 @@
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"],
+  "bark": "bow-wow"
+};
+
+delete ourDog.bark;
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+delete  myDog.tails; //to delete any properties of my Object.
+// Only change code below this line.
