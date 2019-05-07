107c9b4f66a9c54f4aedc8b44201de13c9119c9b
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-06 20:30:49 -0600

    Update 81-Accessing_Object_Properties_with_Dot_Notation.js
    Accessing Object Properties with Dot Notation

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,13 @@
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
+
+//declare var and use dot notacion (.) to change and access the object.
