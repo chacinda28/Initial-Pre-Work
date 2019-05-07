95bd6ffdcf9fae39822b7f00ca286581457b31f8
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-06 19:43:47 -0600

    Update 77-Returning_Boolean_Values_from_Functions.js
    Returning Boolean Values from Functions using the function isless.

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,9 @@
+function isLess(a, b) {
+  // Fix this code
+  return (a < b);
+  }
+
+// Change these values to test
+isLess(15,10);
+
+//This is the better code (using the function isless) to return true o false.
