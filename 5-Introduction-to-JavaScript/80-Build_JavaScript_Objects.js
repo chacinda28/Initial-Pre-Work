716ceaea3316961e4252b8a670b2c8a24f83224f
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-06 20:23:18 -0600

    Update 80-Build_JavaScript_Objects.js
    Build JavaScript Objects

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,18 @@
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name" : "dog",
+  "legs" : 4,
+  "tails" : 1,
+  "friends" : ["everybody"]
+};
+
+//First lesson to build objets in javascript. define and put on all properties about the objects.
