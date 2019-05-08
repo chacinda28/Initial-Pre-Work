632c4e8760456467eb4591b24470475f8be88b51
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-08 15:59:08 -0600

    Update 12-Use-Destructuring-Assignment-to-Assign-Variables-from-Objects.js
    Destructuring Assignment to Assign Variables from Objects

---
+++ 6-Introduction-to-the-ES6-Challenges/12-Use-Destructuring-Assignment-to-Assign-Variables-from-Objects.js
@@ -0,0 +1,38 @@
+//Exercise
+/*
+
+const AVG_TEMPERATURES = {
+  today: 77.5,
+  tomorrow: 79
+};
+
+function getTempOfTmrw(avgTemperatures) {
+  "use strict";
+  // change code below this line
+  const tempOfTomorrow = undefined; // change this line   
+  // change code above this line
+  return tempOfTomorrow;
+}
+
+console.log(getTempOfTmrw(AVG_TEMPERATURES)); // should be 79
+
+*/
+
+
+//Solve 
+
+const AVG_TEMPERATURES = {
+  today: 77.5,
+  tomorrow: 79
+};
+
+function getTempOfTmrw(avgTemperatures) {
+  "use strict";
+  // change code below this line
+  const { tomorrow:  tempOfTomorrow} = avgTemperatures
+  //const ( tomorrow: tempOfTomorrow ) = avgTemperatures // change this line
+  // change code above this line
+  return tempOfTomorrow;
+}
+
+console.log(getTempOfTmrw(AVG_TEMPERATURES)); // should be 79
