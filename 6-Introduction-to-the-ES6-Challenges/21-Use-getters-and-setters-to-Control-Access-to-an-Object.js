bfff85573788d343ab5ef3b7da747030aacc96be
chacinda28 <49892354+chacinda28@users.noreply.github.com>
2019-05-08 19:59:06 -0600

    Update 21-Use-getters-and-setters-to-Control-Access-to-an-Object.js
    Use getters and setters to Control Access to an Object

---
+++ 6-Introduction-to-the-ES6-Challenges/21-Use-getters-and-setters-to-Control-Access-to-an-Object.js
@@ -0,0 +1,18 @@
+function makeClass() {
+  "use strict";
+  /* Alter code below this line */
+class Thermostat {
+  constructor(Fahrenheit) {
+    this.Thermostat=temp;
+  }
+}
+
+  /* Alter code above this line */
+  return Thermostat;
+}
+const Thermostat = makeClass();
+const thermos = new Thermostat(76); // setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in C
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in C
+console.log(thermos.temperature);
